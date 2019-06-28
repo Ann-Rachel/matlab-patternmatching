48837452fcd74719bc2a29cd7714553551fdfacd
Ann-Rachel <33717272+Ann-Rachel@users.noreply.github.com>
2019-06-28 16:57:58 +0200

    Update patternExactly.m

--- patternExactly.m
+++ patternExactly.m
@@ -5,7 +5,7 @@ match = 0;
 for i = 1 : size(A,1)-size(B,1)
     zeilenindex =  patternExactlyInRow(A(i,:),B(1,:));
     for n = 1 : size(zeilenindex,2)
-        hilfe =  A(i: i + size(B,2)-1, zeilenindex(n): zeilenindex(n)+ size(B,2)-1);
+        hilfe =  A(i: i + size(B,1)-1, zeilenindex(n): zeilenindex(n)+ size(B,2)-1);
         hilfe2 = (hilfe == B);
         hilfe3 = sum(hilfe2,'all');
         if hilfe3 == (size(B,1)*size(B,2))
